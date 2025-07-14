5596ba0d8a806851888ddebcbd51ae99ad5e73f1
Dylann02 <hopkinslucka@gmail.com>
2025-07-14 15:35:15 +0300

    projetv1

--- inc/connexion.php
+++ inc/connexion.php
@@ -1,7 +1,7 @@
 <?php 
     function dbconnect(){
-        if($db =mysqli_connect('localhost' , 'root' , '' , 'projet_final')){
-        //  if($db =mysqli_connect('localhost','ETU003952' ,'QGzd0wVR' ,'db_s2_ETU003952')){
+        // if($db =mysqli_connect('localhost' , 'root' , '' , 'projet_final')){
+         if($db =mysqli_connect('localhost','ETU003952' ,'QGzd0wVR' ,'db_s2_ETU003952')){
             return $db;
         }
     }
