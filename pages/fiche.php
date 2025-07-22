ff9bdb3092d6537032610ea52b56f3b8d2387f48
Dylann02 <hopkinslucka>
2025-07-22 22:50:02 +0300

    exam7

--- pages/fiche.php
+++ pages/fiche.php
@@ -4,7 +4,7 @@ session_start();
 $image=get_image($_GET['id']);
 ?><!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
